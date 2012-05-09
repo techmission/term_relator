9e5d991047ee2fada8ba7a9539da3518b341d29d
Evan Donovan <evan@evandonovan.org>
2012-05-09 19:34:04 -0400

    Filter by vocabulary.

--- terms_linker.php
+++ terms_linker.php
@@ -5,11 +5,21 @@ global $DB;
 define('IS_CLI', PHP_SAPI === 'cli');
 define('FILE_TERMS', dirname(__FILE__) . '/terms-list.txt');
 
+define('VID_SVC_AREA', 32);
+define('VID_KEYWORD', 350);
+
 // Load the class for doing the inserts to the database.
 require_once(dirname(__FILE__) . '/jobsdb.class.php');
 
 // Temporarily display runtime errors to the screen.
 ini_set('display_errors', TRUE);
+
+// To get the information from term relation: 
+//
+// select td1.name, td2.name from um_term_relation tr 
+//join um_term_data td1 on td1.tid = tr.tid1
+// join um_term_data td2 on td2.tid = tr.tid2;
+
 
 if (IS_CLI) {
  $terms = array();
@@ -27,9 +37,9 @@ if (IS_CLI) {
     if($svc_area == '"Sound') {
       $svc_area = 'Sound, Lights, Video Ministry';
     }
-    $tid1 = get_term_tid($svc_area);
+    $tid1 = get_term_tid($svc_area, VID_SVC_AREA);
     foreach($terms as $term) {
-     $tid2 = get_term_tid($term);
+     $tid2 = get_term_tid($term, VID_KEYWORD);
      if(is_numeric($tid1) && is_numeric($tid2)) {
        $inserted = write_relation($tid1, $tid2);
        $num_inserted++;
@@ -47,7 +57,7 @@ if (IS_CLI) {
 }
 
 echo "Attempts: " . $num_attempts . "\n";
-echo "Successes: " . $num_inserted . "\n";
+echo "Successes: " . $num_inserted . "\n"; // Why is this wrong?
 echo "Failures: " . $num_failures . "\n";
 exit();
 
@@ -69,11 +79,12 @@ function connect_to_db() {
  return;
 }
 
-function get_term_tid($term_name) {
+function get_term_tid($term_name, $vid) {
  global $DB;
  if(!empty($term_name)) {
-  $stmt = $DB->dbh->prepare('SELECT tid FROM um_term_data WHERE name = :name');
+  $stmt = $DB->dbh->prepare('SELECT tid FROM um_term_data WHERE name = :name AND vid = :vid');
   $stmt->bindValue(':name', $term_name, PDO::PARAM_STR);
+  $stmt->bindValue(':vid', $vid, PDO::PARAM_INT);
   $stmt->execute();
   $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
   if(count($rows) > 0 && $rows[0]['tid']) {
