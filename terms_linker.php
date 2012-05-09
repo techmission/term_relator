224ab6f6d40172a2ea36d4f66f55f42d6853491a
Evan Donovan <evan@evandonovan.org>
2012-05-09 19:20:09 -0400

    Added more debugging

--- terms_linker.php
+++ terms_linker.php
@@ -32,8 +32,11 @@ if (IS_CLI) {
      $tid2 = get_term_tid($term);
      if(is_numeric($tid1) && is_numeric($tid2)) {
        $inserted = write_relation($tid1, $tid2);
+       $num_inserted = $num_inserted + $inserted;
      }
-     $num_inserted = $num_inserted + $inserted;
+     else {
+       echo "Failed on: " . $svc_area . " | " . $term . "\n";
+     }
      $num_attempts++;
     }
    }
