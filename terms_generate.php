8d0e1219c2f1f8c31b2474aea59feaefeb90dd14
Evan Donovan <evan@evandonovan.org>
2012-05-09 18:02:59 -0400

    Debug opening file

--- terms_generate.php
+++ terms_generate.php
@@ -1,12 +1,16 @@
 <?php
 
 define('IS_CLI', PHP_SAPI === 'cli');
-define('FILE_TERMS', 'terms-list.txt');
-define('FILE_SAVE_TERMS', 'terms-list-new.txt');
+define('FILE_TERMS', dirname(__FILE__) . '/terms-list.txt');
+define('FILE_SAVE_TERMS', dirname(__FILE__) . '/terms-list-new.txt');
 
 if (IS_CLI) {
  $terms = array();
- if($read_handle = fopen(FILE_TERMS, 'r') && $write_handle = fopen(FILE_SAVE_TERMS, 'a+')) {
+ $read_handle = fopen(FILE_TERMS, 'r');
+ $write_handle = fopen(FILE_SAVE_TERMS, 'a+');
+ var_dump($read_handle);
+ var_dump($write_handle);
+ if($read_handle && $write_handle) {
    while(($line = fgets($read_handle)) !== FALSE) {
      echo($line);
      $terms = explode(',', $line);
